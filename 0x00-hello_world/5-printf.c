9f2ebd06f24e1f98f96564cbd6206e8414d598e8
Dayvid007 <Sorunkedavid18@gmail.com>
2022-10-13 18:15:12 -0700

    task5

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+/**
+ * main - starting point of the program
+ * Description: main -Prints out a string stdout
+ * Return: 0 if success
+ */
+int main(void)
+{
+printf("with proper grammar, but the outcome is a piece of art,\n");
+return (0);
+}
