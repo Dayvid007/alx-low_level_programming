de44479f59ff2f7add81f7589a5a3dc4027a4d1a
Dayvid007 <Sorunkedavid18@gmail.com>
2022-10-14 19:09:09 -0700

    task2

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+/**
+ * main -Starting point of the program
+ * Description: main -Prints out a string to stdout
+ * Return: 0 if success
+ */
+/* betty style doc for function main goes there */
+int main(void)
+{
+int ch;
+for (ch = 'a' ch <= 'z'; ch++)
+	putchar(ch);
+	putchar('\n');
+return (0);
+}
