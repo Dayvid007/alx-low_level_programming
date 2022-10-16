3be85f208d8e25ee8c44fe8a0cc045661f1ee649
Dayvid007 <Sorunkedavid18@gmail.com>
2022-10-16 14:25:25 -0700

    task5

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+/**
+ * main - Prints the alphabet in lowercase.
+ * Description: main -Prints out a string to stdout
+ * Return: Always 0.
+ */
+
+int main(void)
+
+{
+int numb
+	for (numb = '0'; numb < '10'; numb++)
+		printf("%d\n", numb);
+return (0);
+}
