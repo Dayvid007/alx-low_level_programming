5ddb2ac5a1d08de77142ec114c167c7562e0f704
Dayvid007 <Sorunkedavid18@gmail.com>
2022-10-18 06:50:52 -0700

    _putchar

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+/**
+ * main - Prints the alphabet in lowercase.
+ * Description: main -Prints out a string to stdout
+ * Return: Always 0.
+ */
+
+int main(void)
+{
+	char chr = '_putchar';
+
+	 putchar(chr);
+	 putchar('\n');
+return (0);
+}
+
