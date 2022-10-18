615aef67d279f37b7f77178b01b7a5d9790695c6
Dayvid007 <Sorunkedavid18@gmail.com>
2022-10-17 20:03:27 -0700

    task11

---
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -0,0 +1,37 @@
+#include <stdio.h>
+/**
+ * main - Prints the alphabet in lowercase.
+ * Description: main -Prints out a string to stdout
+ * Return: Always 0.
+ */
+int main(void)
+{
+	int digit1 = '0';
+	int digit2 = '0';
+	int digit3 = '0';
+
+	for (digit3 = 0; digit3 <= 9; digit3++)
+{
+	for (digit2 = 0; digit2 <= 9; digit2++)
+{
+	for (digit1 = 0; digit1 <= 9; digit1++)
+{
+	if (!((digit1 == digit2) || (digit3 == digit2) ||
+	(digit2 > digit1) || (digit3 > digit2)))
+	{
+		putchar(digit3);
+		putchar(digit2);
+		putchar(digit1);
+	}
+	if (!(digit1 == '9' && digit2 == '8' && digit3 == '7'))
+		{
+		putchar(',');
+		putchar(' ');
+
+		}
+	}
+}
+
+	putchar('\n');
+return (0);
+}
