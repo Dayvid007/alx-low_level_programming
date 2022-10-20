ba4ec8782bb509b3edc33048f3eb9984a5d1da13
Dayvid007 <Sorunkedavid18@gmail.com>
2022-10-20 05:55:42 -0700

    digit_checks

---
+++ 0x04-more_functions_nested_loops/1-isdigit.c
@@ -0,0 +1,14 @@
+#include "main.h"
+/**
+ *_isdigit - prints numbers in upper case
+ *@c:  is the int to be checked
+ *Return: returns 1 if successful, else returns 0
+ */
+int _isdigit(int c)
+{
+	if (c >= '0' && c <= '9')
+
+	return (1);
+	else
+		return (0);
+}
