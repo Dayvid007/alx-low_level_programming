e7ae486bba0e926804dc2f01570fa0fdc5b2ddf0
Dayvid007 <Sorunkedavid18@gmail.com>
2022-10-19 18:42:13 -0700

    times_table

---
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -0,0 +1,30 @@
+#include "main.h"
+/**
+ *  times_table - prints every minute of the d
+ *
+ */
+void times_table(void)
+{
+	int num1, num2, prod;
+
+	for (num1 = 0; num1 <= ; ++num1)
+
+	{
+		_putchar(48);
+
+		for (num2 = 0; num2 <= 9; ++num)
+		{
+			_putchar(',');
+			_putchar(' ');
+
+			prod = num1 * num2;
+			if (prod <= 9)
+			_putchar(' ');
+			else
+			_putchar((prod / 10) + 48);
+			_putcharr((prod / 10) + 48);
+		}
+		_putchar('\n');
+	}
+
+}
