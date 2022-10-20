add1174f211151fdf29f73a1fb7cc9a007a14dfe
Dayvid007 <Sorunkedavid18@gmail.com>
2022-10-20 07:18:51 -0700

    print numbers

--- 0x04-more_functions_nested_loops/3-print_numbers.c
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -4,13 +4,10 @@
  */
 void print_numbers(void)
 {
-	int c = 0;
+	int c;
 
-	while (c <= 9)
+	for (c = 0; c <= 9; c++)
 
-	{
-		_putchar('0' + c);
-		c++;
-	}
+	_putchar((c % 10) + '0');
 	_putchar('\n');
 }
