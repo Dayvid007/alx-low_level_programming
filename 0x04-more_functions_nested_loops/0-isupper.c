a1a8b65d89038071dcab1b4bdae10eeb8ac4d9c0
Dayvid007 <Sorunkedavid18@gmail.com>
2022-10-20 05:37:54 -0700

    upper alphabets

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,14 @@
+#include "main.h"
+/**
+ *_isupper - prints alphabets in upper case
+ *@c:  is the character to be checked
+ *Return: returns 1 if successful, else returns 0
+ */
+int _isupper(int c)
+{
+	if (c >= 'A' && c <= 'Z')
+
+	return (1);
+	else
+		return (0);
+}
---
+++ 0x04-more_functions_nested_loops/README.md
@@ -0,0 +1,1 @@
+Thi is the main directory for 0x04-more_functions_nested_loops
