f07d41fdfc1d14f3b2342b7e9c521416e78c4775
Dayvid007 <sorunkedavid18@gmail.com>
2023-09-18 16:25:10 -0700

    Task2

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/**
+ * _strlen-The function name that is used to count the number of strings
+ * @s: the value of the string to get the length o
+ * Return: The avalue shpuld return the number of char if succesful
+ */
+int _strlen(char *s)
+{
+int i;
+
+int count;
+
+for (i = 0; *s[i] != '\0'; i++)
+{
+count++;
+}
+return (0);
+}
