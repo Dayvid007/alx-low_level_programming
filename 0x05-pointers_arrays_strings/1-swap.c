d7007ad2e09dbcdeef668c0519930c733fcc4b74
Dayvid007 <sorunkedavid18@gmail.com>
2023-09-18 14:13:53 -0700

    task1

---
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -0,0 +1,12 @@
+#include "main.h"
+/**
+ * swap_int - The function that wil change the pointer variables
+ * @a: The first pointer variable to be changed
+ * @b: The Second pointer bariable to be changed
+ */
+void swap_int(int *a, int *b)
+{
+*a = 42;
+
+*b = 98;
+}
