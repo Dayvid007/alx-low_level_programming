ae54e93f5fee214c71bdaf89b114533581fe3f9c
Dayvid007 <sorunkedavid18@gmail.com>
2023-09-19 13:56:34 -0700

    Task4

--- 0x05-pointers_arrays_strings/4-print_rev.c
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -7,13 +7,17 @@ void print_rev(char *s)
 {
 int length = 0;
 
+int i = length - 1;
+
 while (s[length] != '\0')
 {
 length++;
 }
-for (int i = length - 1; i >= 0; i--)
+
+while (i >= 0)
 {
-_putchar(s[length]);
+_putchar(s[i]);
+i--;
 }
 _putchar('\n');
 }
