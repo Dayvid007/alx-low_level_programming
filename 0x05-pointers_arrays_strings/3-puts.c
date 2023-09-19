af664065ddbf795acd17ad09dae2bb258f0f6a2f
Dayvid007 <sorunkedavid18@gmail.com>
2023-09-19 11:27:59 -0700

    task3

--- 0x05-pointers_arrays_strings/3-puts.c
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -1,11 +1,11 @@
 #include "main.h"
 /**
  *_puts- The name of the function that prints the pointer variable
- *@s: the parameter of the function
+ *@str: the parameter of the function
  *Return: The return value of the function is not going to ecist since i
  */
 void _puts(char *str)
-{       
+{
 int i;
 
 for (i = 0; str[i] != '\0'; i++)
