689c21b09c9730ebbbedbc66d59b8a7ef304cee3
Dayvid007 <sorunkedavid18@gmail.com>
2023-09-19 16:26:09 -0700

    task6

---
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -0,0 +1,29 @@
+#include "main.h"
+/**
+ * rev_string- The name of the function that is about
+ *@s:one of the function of the parameter
+ */
+void rev_string(char *s)
+{
+int i;
+
+int len = 0, index;
+
+for (i = 0; s[i] != '\0'; i++)
+{
+_putchar(s[i]);
+}
+_putchar('\n');
+while (*s != '\0')
+{
+len++;
+s++;
+}
+s--;
+
+for (index = len; index > 0; index--)
+{
+_putchar(*s);
+s--;
+}
+}
