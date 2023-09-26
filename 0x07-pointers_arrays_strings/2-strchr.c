8e0362f1e506f3f8d2269389dc487cbf02324cc2
Dayvid007 <sorunkedavid18@gmail.com>
2023-09-26 11:58:09 -0700

    ask 2

--- 0x07-pointers_arrays_strings/2-strchr.c
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -10,11 +10,11 @@
 char *_strchr(char *s, char c)
 {
 unsigned int i;
-for (i = 0; s[i] != '\0'; i++)
+for (i = 0; s[i] >= '\0'; i++)
 {
 if (s[i] == c)
 {
-return ((char*)&s[i]);
+return (s + i);
 }
 }
 return (NULL);
