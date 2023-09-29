a1b477b2a45853bdae96a81e17e653b943b57a29
Dayvid007 <sorunkedavid18@gmail.com>
2023-09-28 17:42:00 -0700

    task 0

---
+++ 0x08-recursion/0-puts_recursion.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/**
+ * _puts_recursion - The function that prints a string
+ * @s: The parameter of the funtion
+ * Return: The return vlaue shuld be 0 if succes
+ */
+void _puts_recursion(char *s)
+{
+if (*s)
+{
+_putchar(*s);
+_puts_recursion(s + 1);
+}
+else
+{
+	_putchar('/n');
+}
+}
