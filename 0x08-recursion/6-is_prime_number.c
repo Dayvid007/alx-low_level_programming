5f0ca428d2cfc22e95aaf7cb199d4fd25729914d
Dayvid007 <sorunkedavid18@gmail.com>
2023-09-29 16:45:56 -0700

    task 6

---
+++ 0x08-recursion/6-is_prime_number.c
@@ -0,0 +1,47 @@
+#include "main.h"
+
+int final_prime(int n, int k);
+
+/**
+ * is_prime_number - a function that returns a prime number.
+ * @n: input value of n.
+ * Return: if true return 1, otherwisse 0
+ */
+
+int is_prime_number(int n)
+{
+	if (n <= 1)
+	{
+		return (0);
+	}
+	return (final_prime(n, n - 1));
+}
+
+/**
+ * final_prime - a function that returns a prime number.
+ * @n: input value of n.
+ * @k: iterator
+ * Return: if true return 1, otherwisse 0
+ */
+
+int final_prime(int n, int k)
+{
+	if (i == 1)
+	{
+		return (1);
+	}
+	if (n % k == 0 && k > 0)
+	{
+		return (0);
+	}
+	return (actual_prime(n, k - 1));
+	if (k == 1)
+	{
+		return (1);
+	}
+	if (n % k == 0 && k > 0)
+	{
+		return (0);
+	}
+	return (final_prime(n, k - 1));
+}
