9c6272fac489308c79a66dbccadbc7fc4ee49508
Dayvid007 <sorunkedavid18@gmail.com>
2023-10-04 09:06:06 -0700

    task0

---
+++ 0x09-static_libraries/2-strchr.c
@@ -0,0 +1,21 @@
+#include "main.h"
+#include <stddef.h>
+/**
+ * *_strchr - a function that copies memory area.
+ * @s: Tje memory area to be searched for
+ * @c: The character that would be searched
+ * Return: The value of the pointer if true and Null if false
+ */
+
+char *_strchr(char *s, char c)
+{
+unsigned int i;
+for (i = 0; s[i] >= '\0'; i++)
+{
+if (s[i] == c)
+{
+return (s + i);
+}
+}
+return (NULL);
+}
