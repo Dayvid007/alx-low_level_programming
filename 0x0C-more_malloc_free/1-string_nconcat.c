e100b3630d05c2e4fb4c65c8fe7f7123c9d15fd0
Dayvid007 <sorunkedavid18@gmail.com>
2023-10-07 19:01:33 -0700

    task1

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -10,42 +10,43 @@
  */
 char *string_nconcat(char *s1, char *s2, unsigned int n)
 {
-	unsigned int i = 0, j = 0, len1 = 0, len2 = 0;
-
-	char *result;
+	unsigned int i = 0, j = 0, k = 0, l = 0;
+	char *str;
 
 	if (s1 == NULL)
-	{
 		s1 = "";
-	}
 	if (s2 == NULL)
-	{
 		s2 = "";
-	}
-	for (; s1 && s1[len1]; len1++)
-	for (; s2 && s2[len2]; len2++)
-	if (n < len2)
-		result = malloc(sizeof(char) * (len1 + n + 1));
-	else
-	result = malloc(sizeof(char) * (len1 + len2 + 1));
 
-	if (result == NULL)
-	{
+	while (s1[i])
+		i++;
+
+	while (s2[k])
+		k++;
+
+	if (n >= k)
+		l = i + k;
+	else
+		l = i + n;
+
+	str = malloc(sizeof(char) * l + 1);
+	if (str == NULL)
 		return (NULL);
-	}
-	for (; i < len1; i++)
+
+	k = 0;
+	while (j < l)
 	{
-		result[i] = s1[i];
+		if (j <= i)
+			str[j] = s1[j];
+
+		if (j >= i)
+		{
+			str[j] = s2[k];
+			k++;
+		}
+		j++;
 	}
-	for (; n < len2 && i < (len1 + n); i++, j++)
-	{
-		result[i] = s2[j];
-	}
-	for (; n >= len2 && i < (len1 + len2); i++, j++)
-	{
-		result[i] = s2[j];
-	}
-	result[i] = '\0';
-	return (result);
+	str[j] = '\0';
+	return (str);
 }
 
