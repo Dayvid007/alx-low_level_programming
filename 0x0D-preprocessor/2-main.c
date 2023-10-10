0fb554555108d3341b39634effea6d92b323f7ad
Dayvid007 <sorunkedavid18@gmail.com>
2023-10-10 10:16:37 -0700

    task0

---
+++ 0x0D-preprocessor/2-main.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+/**
+ * main - The funtion thta prints the name of file
+ * Return: 0 if the program is successful
+ */
+int main(void)
+{
+	printf("%s\n", __FILE__);
+	return (0);
+}
