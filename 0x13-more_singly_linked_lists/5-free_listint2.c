464adfce3603965d19a5fd4509e93a95da8a5bd3
Dayvid007 <sorunkedavid18@gmail.com>
2023-10-25 11:04:14 -0700

    task4

---
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -0,0 +1,22 @@
+#include "lists.h"
+/**
+ * free_listint2 - The function that frees list
+ * @head: The pointer to the param head
+ * Return: The value of the node.
+ */
+void free_listint2(listint_t *head)
+{
+	listint_t *pointerf;
+
+	if (head == NULL)
+	{
+		return;
+	}
+	while (*head)
+	{
+		pointerf = (*head)->next;
+		free(*head);
+		*head = pointerf;
+	}
+	*head = NULL;
+}
