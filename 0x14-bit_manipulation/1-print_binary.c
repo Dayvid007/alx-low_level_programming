9070c2d0c82dcf3bafc26c043c61a061c6bcab59
Dayvid007 <sorunkedavid18@gmail.com>
2023-10-27 13:51:47 -0700

    Task0

---
+++ 0x14-bit_manipulation/1-print_binary.c
@@ -0,0 +1,52 @@
+#include "main.h"
+
+/**
+ * _pow - func calculates (base ^ power)
+ * @base: base of the exponent
+ * @power: power of the exponent
+ *
+ * Return: value of (base ^ power)
+ */
+
+unsigned long int _pow(unsigned int base, unsigned int power)
+{
+	unsigned long int number;
+	unsigned int long;
+
+	number = 1;
+	for (long = 1; long <= power; long++)
+	{
+		number *= base;
+	}
+	return (number);
+}
+
+/**
+ * print_binary - prints a number in binary notation
+ * @n: number to print
+ *
+ * Return: void
+ */
+
+void print_binary(unsigned long int n)
+{
+	unsigned long int divider, checkers;
+	char flags;
+
+	flags = 0;
+	divider = _pow(2, sizeof(unsigned long int) * 8 - 1);
+	while (divider != 0)
+	{
+		checkers = n & divider;
+		if (checkers == divider)
+		{
+			flags = 1;
+			_putchar('1');
+		}
+		else if (flags == 1 || divider == 1)
+		{
+			_putchar('0');
+		}
+		divider >>= 1;
+	}
+}
--- 0x14-bit_manipulation/main.h
+++ 0x14-bit_manipulation/main.h
@@ -1,7 +1,8 @@
 #ifndef MAIN_H
 #define MAIN_H
 #include <stdio.h>
-
+int _putchar(char c);
+unsigned long int _pow(unsigned int base, unsigned int power);
 unsigned int binary_to_uint(const char *b);
 void print_binary(unsigned long int n);
 int get_bit(unsigned long int n, unsigned int index);
