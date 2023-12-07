5b59d126f939cc6a8dc59dbe11f82c8e20be90c0
Dayvid007 <sorunkedavid18@gmail.com>
2023-12-07 14:40:56 -0800

    Tasks

--- 0x17-doubly_linked_lists/7-insert_dnodeint.c
+++ 0x17-doubly_linked_lists/7-insert_dnodeint.c
@@ -1,5 +1,4 @@
 #include "lists.h"
-
 /**
  * insert_dnodeint_at_index - inserts a new node at
  * a given position
@@ -8,18 +7,14 @@
  * @n: value of the new node
  * Return: the address of the new node, or NULL if it failed
  */
-
 dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 {
-	dlistint_t *new;
-	dlistint_t *head;
+	dlistint_t *new, *head;
 	unsigned int i;
 
 	new = NULL;
 	if (idx == 0)
-	{
 		new = add_dnodeint(h, n);
-	}
 	else
 	{
 		head = *h;
@@ -53,6 +48,5 @@ dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 			i++;
 		}
 	}
-
 	return (new);
 }
